c2352ba7dd072259068351e5f57dec1f030d9518
codeamac01 <codeamac01@gmail.com>
2015-10-30 18:14:37 -0600

    method that receives an integer as argument (number) and returns a roman number

---
+++ CC_Ruby_romanos.rb
@@ -0,0 +1,64 @@
+=begin
+Method: to_roman.
+This method receives an integer as argument (number) and returns a roman number.
+=end
+  
+
+def to_roman(number)
+   #Hash is initialized with arabic and roman number
+   roman =
+      {
+        1000 => "M",
+        900 => "CM",
+        500 => "D",
+        400 => "CD",
+        100 => "C",
+        90 => "XC",
+        50 => "L",
+        40 => "XL",
+        10 => "X",
+        9 => "IX",
+        5 => "V",
+        4 => "IV",
+        1 => "I"
+      }
+
+   #'number_arabic' variable is initialized'   
+   number_arabic = ""
+   # it evaluates 'roman' Hash 
+   roman.each { |k,n| 
+      #loop to get romam number
+      while number >= k
+         #it subtracts key Hash to number 
+         number = number - k
+         #roman numbers are converted to arabic numbers
+         number_arabic<<roman[k]
+      end
+   }
+   #result, string with roman number
+   number_arabic
+end
+
+
+a = to_roman(1) == "I"
+b = to_roman(3) == "III"  
+c = to_roman(4) == "IV"
+d = to_roman(9) == "IX"
+e = to_roman(13) == "XIII"
+f = to_roman(14) == "XIV"
+g = to_roman(944) == "CMXLIV"
+h = to_roman(1453) == "MCDLIII"
+i = to_roman(1646) == "MDCXLVI"
+
+
+puts "|valid |input | expected | actual"
+puts "|------|------|----------|-------"
+puts "| #{a} |1     | I        | #{to_roman(1)}"
+puts "| #{b} |3     | III      | #{to_roman(3)}"
+puts "| #{c} |4     | IV       | #{to_roman(4)}"
+puts "| #{d} |9     | IX       | #{to_roman(9)}"
+puts "| #{e} |13    | XIII     | #{to_roman(13)}"
+puts "| #{f} |14    | XIV      | #{to_roman(14)}"
+puts "| #{g} |944   | CMXLIV   | #{to_roman(944)}"
+puts "| #{h} |1453  | MCDLIII  | #{to_roman(1453)}"
+puts "| #{i} |1646  | MDCXLVI  | #{to_roman(1646)}" 
